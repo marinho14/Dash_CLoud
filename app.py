dc1826c884393eb418b965d39db97289f751929c
marinho14 <marinosebastian5@gmail.com>
2022-11-10 15:31:36 -0500

    up complete

--- app.py
+++ app.py
@@ -11,20 +11,21 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime  
 import plotly.graph_objects as go
-from database import Database_Google
-
+from database import Database
 ###################################################################################
 
-Google= Database_Google("NewDatabase","Sheet1","sheet2")
+
+
+val2show = 10
+val2print = 100
+
+Google= Database("NewDatabase","Sheet1","sheet2","firebase")
 
 color_sliders= "#FFFFFF"
 
-time_stamp_global=[]
-Torque_global=[]
-Dispo_global=[]
 ###################################################################################
-##app.title = 'Utms' ## Tittle in the web page
-##app._favicon = ("assets/favicon.ico") Image in the webpage
+app.title = 'Datos Exoesqueleto' ## Tittle in the web page
+app._favicon = ("iconn.ico") 
 
 app.layout = html.Div(children=[
     html.H1(children='Control Exoesqueleto',className='banner_t'),
@@ -80,6 +81,9 @@ app.layout = html.Div(children=[
 
     dcc.Dropdown(['Hombre', 'Mujer'], 'Hombre',id='gender-dropdown',className='banner_d'),
 
+    #dcc.RadioItems([{ "label": html.Div(['Hombre'], style={'color': 'Gold', 'font-size': 20}),"value":"Hombre"},{ "label": "  Mujer","value":"Mujer"}],
+    #               'Hombre',id='gender-dropdown',className='banner_d', inline=True),
+    
     html.Button('Empezar', n_clicks=0, id='start_butt',className='button1'),
     
 
@@ -90,10 +94,27 @@ app.layout = html.Div(children=[
         ]),
 
         dbc.Col([
+            dcc.Graph(id = 'graph_realidad_virtual', animate = True, className="Graph_izq")
+        ]),
+
+        dbc.Col([
             dcc.Graph(id = 'graph_disposicion_final', animate = True, className="Graph_der"),
         ]),
 
     ],className="Fila_1"),
+
+    dbc.Row([ 
+
+        dbc.Col([
+            dcc.Graph(id = 'graph_int_torque_final', animate = True, className="Graph_izq")
+        ]),
+
+        dbc.Col([
+            dcc.Graph(id = 'graph_cont_torque_final', animate = True, className="Graph_der"),
+        ]),
+
+    ],className="Fila_1"),
+
 
     dcc.Interval(
         id = 'graph-update',
@@ -127,49 +148,177 @@ def change_button_style(n_clicks,emocion,espa,dolor,rv,gender):
         Google.define_in(emocion,espa,dolor,rv,gender,0)
         return True
 
+
+
+
 @app.callback(
     Output("graph_torque_final", "figure"), 
     Output("graph_disposicion_final", "figure"), 
+    Output("graph_int_torque_final", "figure"), 
+    Output("graph_cont_torque_final", "figure"), 
+    Output("graph_realidad_virtual", "figure"), 
+    Output("graph_torque_final", "animate"), 
+    Output("graph_disposicion_final", "animate"), 
+    Output("graph_int_torque_final", "animate"), 
+    Output("graph_cont_torque_final", "animate"), 
+    Output("graph_realidad_virtual", "animate"), 
     Input('graph-update', 'n_intervals'),
 )
 def graph_toqrque(n_intervals):
-    max_row = int(Google.sheet2.acell('A2').value)
+    max_row = Google.get_max()
+    if(max_row>0):
+        min_val = max_row-val2print
+        
+        if(max_row<val2show):
+            val2show_local= max_row
+        else:
+            val2show_local = val2show
+        
+        if(min_val<=0):min_val=1
+        flag_animate = True
+        vec_read = Google.read_data(min_val,max_row)  
+        
+        #[timestamp, emocion,espasticidad,dolor,ambienteRV, disposicion, intensidad_torque, torque_control, torque_Final, stop]
+        time_stamp_global= vec_read[0]
+        Torque_global = vec_read[8]
+        Dispo_global = vec_read[5]
+        Inten_Torque = vec_read[6]
+        Cont_Torque = vec_read[7]
+        Realidad_v = vec_read[4]
+        
 
-    tiempo= Google.sheet1.get("A"+str(1)+":A"+str(max_row))
-    time_stamp_global = list(map(lambda x: datetime.fromtimestamp(float(x[0])), tiempo))
+        data_torque = go.Scatter(
+                x=time_stamp_global,
+                y=Torque_global,
+                name='Scatter',
+                mode= 'lines+markers'
+        )
 
-    Torque= Google.sheet1.get("I"+str(1)+":I"+str(max_row))
-    Torque_global= list(map(lambda x: float(x[0]), Torque))
+        data_dispo = go.Scatter(
+                x=time_stamp_global,
+                y=Dispo_global,
+                name='Scatter',
+                mode= 'lines+markers'
+        )
 
-    Dispo= Google.sheet1.get("G"+str(1)+":G"+str(max_row))
-    Dispo_global= list(map(lambda x: float(x[0]), Dispo))
+        data_int_torq = go.Scatter(
+                x=time_stamp_global,
+                y=Inten_Torque,
+                name='Scatter',
+                mode= 'lines+markers'
+        )
 
-    if len(Torque_global)>10:
-        time_stamp_global=time_stamp_global[:-10]
-        Torque_global=Torque_global[:-10]
-        Dispo_global =Dispo_global[:-10]
+        data_cont_torq = go.Scatter(
+                x=time_stamp_global,
+                y=Cont_Torque,
+                name='Scatter',
+                mode= 'lines+markers'
+        )
 
-    data_torque = go.Scatter(
-            x=time_stamp_global,
-            y=Torque_global,
+        data_realidad = go.Scatter(
+                x=time_stamp_global,
+                y=Realidad_v,
+                name='Scatter',
+                mode= 'lines+markers'
+        )
+
+
+        rango_minimo_x = min(time_stamp_global[-val2show_local-1:-1])
+        rango_maximo_x = max(time_stamp_global[-val2show_local-1:-1])
+
+        rango_minimo_torque= min(Torque_global[-val2show_local-1:-1])
+        rango_maximo_torque = max(Torque_global[-val2show_local-1:-1])
+
+        rango_minimo_dispo= min(Dispo_global[-val2show_local-1:-1])
+        rango_maximo_dispo= max(Dispo_global[-val2show_local-1:-1])
+
+        rango_minimo_int_tor= min(Inten_Torque[-val2show_local-1:-1])
+        rango_maximo_int_tor= max(Inten_Torque[-val2show_local-1:-1])       
+
+        rango_minimo_cont_tor= min(Cont_Torque[-val2show_local-1:-1])
+        rango_maximo_cont_tor= max(Cont_Torque[-val2show_local-1:-1])
+
+        rango_minimo_realidad= min(Realidad_v[-val2show_local-1:-1])
+        rango_maximo_realidad= max(Realidad_v[-val2show_local-1:-1])
+
+    else:
+        flag_animate = False
+        data_torque = go.Scatter(
+            x=[],
+            y=[],
             name='Scatter',
             mode= 'lines+markers'
-    )
+        )
 
-    data_dispo = go.Scatter(
-            x=time_stamp_global,
-            y=Dispo_global,
+        data_dispo = go.Scatter(
+            x=[],
+            y=[],
             name='Scatter',
             mode= 'lines+markers'
-    )
+        )
+
+        data_int_torq = go.Scatter(
+                x=[],
+                y=[],
+                name='Scatter',
+                mode= 'lines+markers'
+        )
+
+        data_cont_torq = go.Scatter(
+                x=[],
+                y=[],
+                name='Scatter',
+                mode= 'lines+markers'
+        )
+
+        data_realidad = go.Scatter(
+                x=[],
+                y=[],
+                name='Scatter',
+                mode= 'lines+markers'
+        )
+
+        time_stamp_global=[0]
+        Torque_global = [0]
+        Dispo_global = [0]
+        Inten_Torque = [0]
+        Cont_Torque = [0]
+        Realidad_v = [0]
+
+        rango_minimo_x = 0
+        rango_maximo_x = 1
+
+        rango_minimo_torque= 0
+        rango_maximo_torque = 1
+
+        rango_minimo_dispo= 0
+        rango_maximo_dispo= 1
+
+        rango_minimo_int_tor= 0
+        rango_maximo_int_tor= 1     
+
+        rango_minimo_cont_tor= 0
+        rango_maximo_cont_tor= 1
+
+        rango_minimo_realidad= 0
+        rango_maximo_realidad= 1
 
     return {'data'   : [data_torque],
-            'layout' : go.Layout(xaxis=dict(range=[min(time_stamp_global),max(time_stamp_global)],color="#f1f1f1"),yaxis = dict(range = [min(Torque_global),max(Torque_global)],color="#f1f1f1"),
+            'layout' : go.Layout(xaxis=dict(range=[rango_minimo_x,rango_maximo_x],color="#f1f1f1"),yaxis = dict(range = [rango_minimo_torque,rango_maximo_torque],color="#f1f1f1"),
             title=dict(text="Grafica Torque",font=dict(color="#f1f1f1")), plot_bgcolor ='#141316',paper_bgcolor='#818d9b')
             },{'data'   : [data_dispo],
-            'layout' : go.Layout(xaxis=dict(range=[min(time_stamp_global),max(time_stamp_global)],color="#f1f1f1"),yaxis = dict(range = [min(Dispo_global),max(Dispo_global)],color="#f1f1f1"),
+            'layout' : go.Layout(xaxis=dict(range=[rango_minimo_x,rango_maximo_x],color="#f1f1f1"),yaxis = dict(range = [rango_minimo_dispo,rango_maximo_dispo],color="#f1f1f1"),
             title=dict(text="Grafica Disposición",font=dict(color="#f1f1f1")), plot_bgcolor ='#141316',paper_bgcolor='#818d9b')
-            }
+            },{'data'   : [data_int_torq],
+            'layout' : go.Layout(xaxis=dict(range=[rango_minimo_x,rango_maximo_x],color="#f1f1f1"),yaxis = dict(range = [rango_minimo_int_tor,rango_maximo_int_tor],color="#f1f1f1"),
+            title=dict(text="Grafica Intensidad Torque",font=dict(color="#f1f1f1")), plot_bgcolor ='#141316',paper_bgcolor='#818d9b')
+            },{'data'   : [data_cont_torq],
+            'layout' : go.Layout(xaxis=dict(range=[rango_minimo_x,rango_maximo_x],color="#f1f1f1"),yaxis = dict(range = [rango_minimo_cont_tor,rango_maximo_cont_tor],color="#f1f1f1"),
+            title=dict(text="Grafica Control Torque",font=dict(color="#f1f1f1")), plot_bgcolor ='#141316',paper_bgcolor='#818d9b')
+            },{'data'   : [data_realidad],
+            'layout' : go.Layout(xaxis=dict(range=[rango_minimo_x,rango_maximo_x],color="#f1f1f1"),yaxis = dict(range = [rango_minimo_realidad,rango_maximo_realidad],color="#f1f1f1"),
+            title=dict(text="Grafica Ambiente Virtual",font=dict(color="#f1f1f1")), plot_bgcolor ='#141316',paper_bgcolor='#818d9b')
+            },flag_animate,flag_animate,flag_animate,flag_animate,flag_animate
 
 
 
--- database.py
+++ database.py
@@ -4,20 +4,41 @@ import pandas as pd
 from datetime import datetime
 import time
 from gspread_dataframe import set_with_dataframe
+import firebase_admin
 
-class Database_Google:
+import firebase_admin
+from firebase_admin import credentials as cre_fire
+from firebase_admin import db
 
-    def __init__(self, Database,sheet_1,sheet_2):
-        self.Database= Database
-        self.sheet_1 = sheet_1
-        self.sheet_2 = sheet_2
-        scope = ['https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive"]
-        credentials = ServiceAccountCredentials.from_json_keyfile_name("exoesquelet-eafit-sheet-bcb340f88950.json", scope)
-        client = gspread.authorize(credentials)
-        self.data_base = client.open(Database)
-        self.sheet1 = self.data_base.worksheet(self.sheet_1)
-        self.sheet2 = self.data_base.worksheet(self.sheet_2) 
-        self.cont_out=1
+import math
+
+import json
+
+
+class Database:
+
+    def __init__(self, Database,sheet_1,sheet_2,name_database):
+        if(name_database == "Google"):
+            self.cond = 0
+            self.Database= Database
+            self.sheet_1 = sheet_1
+            self.sheet_2 = sheet_2
+            scope = ['https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive"]
+            credentials = ServiceAccountCredentials.from_json_keyfile_name("exoesquelet-eafit-sheet-bcb340f88950.json", scope)
+            client = gspread.authorize(credentials)
+            self.data_base = client.open(Database)
+            self.sheet1 = self.data_base.worksheet(self.sheet_1)
+            self.sheet2 = self.data_base.worksheet(self.sheet_2) 
+            self.cont_out=1
+        else:
+            self.cond=1
+            cred = cre_fire.Certificate("exoesqueleto-eafit-firebase-adminsdk-ubdid-01f812b4a1.json")
+            firebase_admin.initialize_app(cred, 
+                {'databaseURL':"https://exoesqueleto-eafit-default-rtdb.firebaseio.com"})
+            self.ref_data_soc = db.reference("/data/data_soc")
+            self.ref_max_data = db.reference("/data/maximo_dato")
+            self.ref = db.reference("/data")
+    
     
     def define_in(self,emocion,espasticidad,dolor,ambiente_rv,gender,stop): 
         self.emocion = emocion
@@ -26,13 +47,44 @@ class Database_Google:
         self.ambiente_rv= ambiente_rv
         self.stop = stop
         self.gender=gender
-        self.sheet2.update('A1',[[self.emocion,self.espasticidad,self.dolor,self.ambiente_rv,self.gender,self.stop]])
+        if(self.cond==0):
+            self.sheet2.update('A1',[[self.emocion,self.espasticidad,self.dolor,self.ambiente_rv,self.gender,self.stop]])
+        else:
+            self.ref.update({"data_web":{"emocion":self.emocion,
+                                         "espasticidad":self.espasticidad,
+                                        "dolor":self.dolor,
+                                        "Ambiente_RV":self.ambiente_rv,
+                                        "gender":self.gender,
+                                        "stop":self.stop}
+                             })
+        
+    def get_max(self):
+        if(self.cond==0):
+            max_row = int(self.sheet2.acell('A2').value)-1
+        else:
+            read_data = self.ref_max_data.get()
+            max_row= (read_data['max'])-1
+            self.num_data = read_data['num_data']
+        
+        return max_row
     
-    def define_out(self,df,tam_lista):  ## Se agrega lo del socket
-        #set_with_dataframe(worksheet=self.sheet1, dataframe=df, include_index=False,include_column_header=False, resize=True)
-        df_values = df.values.tolist()
-        self.data_base.values_append('Sheet1', {'valueInputOption': 'RAW'}, {'values': df_values})
-        self.cont_out= self.cont_out+ (tam_lista-1)
-        self.sheet2.update('A2',self.cont_out)
+    def read_data(self,min_val,max_val):
+        
+        if (self.cond==0):
+            data_read =self.sheet1.get("A"+str(min_val)+":K"+str(max_val))   
+            
+            lista_out = list(map(lambda x:list(map(lambda y: float(y),x)) ,data_read))
+            lista_out = pd.DataFrame(lista_out)
+            
+            lista_out[0]= lista_out[0].map(lambda x: datetime.fromtimestamp(x)) 
+        else:
+            datos_leidos = self.ref_data_soc.order_by_child("id").limit_to_last(math.ceil((max_val-min_val+1)/self.num_data)).get()
+            lista_aux = list(map(lambda x: x['tabla'],list(datos_leidos.values())))
+            lista_out = []
+            for i in lista_aux:
+                lista_out.extend(i)
+            lista_out = pd.DataFrame(lista_out)
+            lista_out[0]= lista_out[0].map(lambda x: datetime.fromtimestamp(x)) 
+        return lista_out
 
 
